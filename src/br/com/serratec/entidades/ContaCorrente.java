581337885e5b860804bd095a061478c531e83f6a
Breno Jardim Magrani <breno.jm25@gmail.com>
2022-04-26 17:25:07 -0300

    Atributos Conta

--- src/br/com/serratec/entidades/ContaCorrente.java
+++ src/br/com/serratec/entidades/ContaCorrente.java
@@ -1,5 +1,15 @@
 package br.com.serratec.entidades;
 
-public class ContaCorrente {
+public class ContaCorrente extends Conta {
+	private String tipo;
+	
+	public ContaCorrente(Usuario usuario, int agencia, int idConta, char tipoConta, String tipo) {
+		super(usuario, agencia, idConta, tipoConta);
+		this.tipo = tipo;
+	}
+	
+	public double TributacaoContaCorrente(double i) {
+		
+	}
 
 }
--- src/br/com/serratec/entidades/ContaPoupanca.java
+++ src/br/com/serratec/entidades/ContaPoupanca.java
@@ -1,5 +1,15 @@
 package br.com.serratec.entidades;
 
-public class ContaPoupanca {
+public class ContaPoupanca extends Conta{
+	private String tipo;
+	
+	public ContaPoupanca(Usuario usuario, int agencia, int idConta, char tipoConta) {
+		super(usuario, agencia, idConta, tipoConta);
+		
+	}
+	
+	public double rendimentoPoupanca(double i) {
+		
+	}
 
 }
