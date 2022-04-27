aaef4ec79755f722f40f04decc5e1dcf0e5a7bdf
PedroHMP <102328509+PedroHMoraesPacheco@users.noreply.github.com>
2022-04-27 17:09:49 -0300

    Usuario Autenticacao

--- src/br/com/serratec/entidades/Usuario.java
+++ src/br/com/serratec/entidades/Usuario.java
@@ -1,9 +1,11 @@
 package br.com.serratec.entidades;
 
+import br.com.serratec.excecoes.AutenticacaoLogin;
 import br.com.serratec.excecoes.DocumentoInvalido;
+import br.com.serratec.interfaceUsuario.Autenticacao;
 import br.com.serratec.validador.ValidarCpf;
 
-public abstract class Usuario {
+public abstract class Usuario implements Autenticacao {
 	protected String nome;
 	protected final String cpf;
 	protected String senha;
@@ -13,7 +15,11 @@ public abstract class Usuario {
 		this.cpf = ValidarCpf.validarCpf(cpf);
 		this.senha = senha;
 	}
-
+	public void autenticar(String login) throws AutenticacaoLogin {
+		if(!this.cpf.equals(login) && !this.senha.equals(login)) {
+			throw new AutenticacaoLogin();
+		}
+	}
 	public String getNome() {
 		return nome;
 	}
@@ -25,6 +31,7 @@ public abstract class Usuario {
 	public String getCpf() {
 		return cpf;
 	}
+<<<<<<< Updated upstream
 	public void autenticar(String cpfDigitado, String senhaDigitada ) throws LoginInvalidoException 
         if(!this.cpf.equals(cpfDigitado)  && !this.senha.equals(senhaDigitada)) {
            throw new LoginInvalido();
@@ -36,3 +43,7 @@ public abstract class Usuario {
 
     
 
+=======
+	
+}
+>>>>>>> Stashed changes
