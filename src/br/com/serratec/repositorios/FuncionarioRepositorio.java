3a23f27e539b73f032407378ec5005a82bfd8d07
PedroHMP <102328509+PedroHMoraesPacheco@users.noreply.github.com>
2022-04-28 14:58:19 -0300

    FuncionarioRepositorio

--- src/br/com/serratec/repositorios/FuncionarioRepositorio.java
+++ src/br/com/serratec/repositorios/FuncionarioRepositorio.java
@@ -2,27 +2,27 @@ package br.com.serratec.repositorios;
 
 import java.util.HashMap;
 import br.com.serratec.entidades.Funcionario;
-import br.com.serratec.excecoes.ContaInvalidaException;
+import br.com.serratec.excecoes.ContaInvalida;
 
 public class FuncionarioRepositorio {
 	private static HashMap<String, Funcionario> mapaFuncionarios = new HashMap<String, Funcionario>();
 	
-	public static void adicionaFuncionario(Funcionario funcionario) throws ContaInvalidaException {
+	public static void adicionaFuncionario(Funcionario funcionario){
 		if (mapaFuncionarios.containsKey(funcionario.getCpf())) {
-			throw new ContaInvalidaException();
+			throw new ContaInvalida();
 		}
 		mapaFuncionarios.put(funcionario.getCpf(), funcionario);
 	}
-	public static void removeFuncionario(String Cpf) throws ContaInvalidaException {
+	public static void removeFuncionario(String Cpf) throws ContaInvalida{
 		if (!mapaFuncionarios.containsKey(Cpf)) {
-			throw new ContaInvalidaException();
+			throw new ContaInvalida();
 		}
 		mapaFuncionarios.remove(Cpf);
 	}
-	public static Funcionario retornaFuncionario(String Cpf) throws ContaInvalidaException {
+	public static Funcionario retornaFuncionario(String Cpf){
 		Funcionario funcionarioPesquisado = mapaFuncionarios.get(Cpf);
 		if (funcionarioPesquisado == null) {
-			throw new ContaInvalidaException();
+			throw new ContaInvalida();
 		}
 		return funcionarioPesquisado;
 	}
