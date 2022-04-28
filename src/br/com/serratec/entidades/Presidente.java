388d782378060b7687c9377e671c936ddf02bbde
João Marcos Bernardes <102327585+JM-Bernardes@users.noreply.github.com>
2022-04-28 15:15:18 -0300

    presidente

--- src/br/com/serratec/entidades/Presidente.java
+++ src/br/com/serratec/entidades/Presidente.java
@@ -1,9 +1,10 @@
 package br.com.serratec.entidades;
 
+import br.com.serratec.excecoes.DocumentoInvalido;
+
 public class Presidente extends Funcionario{
 
-	public Presidente(String nome, String cpf, String senha, String cargo) {
+	public Presidente(String nome, String cpf, String senha, String cargo) throws DocumentoInvalido {
 		super(nome, cpf, senha, cargo);
 	}
-	
 }
