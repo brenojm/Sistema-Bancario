1ab5b3ff001415593ae1041dae8c38d41176a4e9
Cauã Stulpen Mello <102328667+cauasmello@users.noreply.github.com>
2022-04-28 15:21:25 -0300

    Cliente

--- src/br/com/serratec/entidades/Cliente.java
+++ src/br/com/serratec/entidades/Cliente.java
@@ -1,5 +1,12 @@
 package br.com.serratec.entidades;
 
-public class Cliente {
+import br.com.serratec.excecoes.DocumentoInvalido;
+
+public class Cliente extends Funcionario {
+
+	public Cliente(String nome, String cpf, String senha, String cargo) throws DocumentoInvalido {
+		super(nome, cpf, senha, cargo);
+		
+	}
 
 }
